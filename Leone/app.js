7f48623f49165e60ed7a1cc22ccf43ff5cb899c6
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2025-03-28 12:33:30 +0000

    punto3

--- Leone/app.js
+++ Leone/app.js
@@ -9,7 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+var usersRouter = require('./routes/users');
+app.use('/rest', usersRouter);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
