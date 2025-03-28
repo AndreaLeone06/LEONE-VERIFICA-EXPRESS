0d4ca57c3c50c8a550a8c6f49c3169fe75e18322
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2025-03-28 12:59:19 +0000

    Punto7

--- Leone/app.js
+++ Leone/app.js
@@ -28,14 +28,17 @@ app.use('/', pagesRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+app.use(function(req, res, next) {
+  if (req.path.startsWith('/rest') && !req.path.startsWith('/pages')) {
+    return next(createError(404, 'Route not found'));
+  }
+  next();
+});
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
