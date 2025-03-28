cbd0147da7076df4bac2fdb7c058a331fb16a294
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2025-03-28 12:49:26 +0000

    punto4

--- Leone/app.js
+++ Leone/app.js
@@ -6,12 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pagesRouter = require('./routes/pages');
 
 var app = express();
 
-var usersRouter = require('./routes/users');
-app.use('/rest', usersRouter);
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -23,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', pagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
