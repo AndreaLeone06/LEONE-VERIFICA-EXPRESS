a3bc99b74d8be46cf38b21440c2cef57c08ed37d
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2025-03-28 12:57:21 +0000

    Punto 5

--- Leone/routes/users.js
+++ Leone/routes/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
-const users = require('../users.json');
 
-router.get('/rest', (req, res) => {
+const users = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+
+router.get('/', (req, res) => {
   res.json(users);
+});
+
+
+router.get('/users/:email', (req, res) => {
+  const user = users.find(u => u.email === req.params.email);
+
+  if (!user) {
+    return res.status(404).json({ error: 'Utente non trovato' });
+  }
+
+  res.json(user);
 });
 
 module.exports = router;
